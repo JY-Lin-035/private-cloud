63719e8acd0635f859861070ff05aff2383395f8
JY-Lin-035 <LJunY035@gmail.com>
2026-06-07 19:18:11 +0800

    chore: clean up temp files from development

--- write_admin.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import pathlib
-
-content = open('/dev/stdin').read()
-pathlib.Path('/home/Jun/safe_box/private-cloud/frontend/src/pages/AdminManagement/AdminManagement.tsx').write_text(content)
-print("Written successfully")
